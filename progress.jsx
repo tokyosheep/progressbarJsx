26d19193a041c1f731d3f57900ed14a9d3aac46c
tokyosheep <kawanoshuji@Pro.local>
2019-06-01 14:41:39 +0900

    modify

--- progress.jsx
+++ progress.jsx
@@ -1,15 +1,21 @@
 
-(function(){
+(function(){ 
     var winObj = new Window("dialog","sample",[100,50,450,300]);
-    winObj.p = winObj.add("progressbar",[20,20,250,130],0,100,"progress");
-    winObj.btn = winObj.add("button",[130,200,325,200+25], "OK!", { name:"ok"});
+    winObj.p = winObj.add("progressbar",[80,20,80+200,20+15],0,200,"progress");
+    winObj.Ok = winObj.add("button",[70,200,70+80,200+25], "OK!");
+    winObj.cancel = winObj.add("button",[200,200,200+80,200+25], "Cancel");
     winObj.center();
     
+    winObj.cancel.onClick = function(){
+        winObj.close(); 
+    }
     
-    winObj.btn.onClick = function(){
+    winObj.Ok.onClick = function(){
+        
+        winObj.active = true;
         for(var i=0;i<200;i++){
-            activeDocument.artLayers.add();
-            winObj.p.value = i/2;
+            winObj.p.value = i;
+            $.sleep(10);  
             winObj.update();
         }
         winObj.close(); 
